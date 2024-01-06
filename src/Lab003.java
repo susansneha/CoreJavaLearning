dcf33a527905422e2ef437d07b36fced5f7f0e46
”susansneha@gmail.com” <susansneha@gmail.com>
2024-01-06 15:34:13 +0530

    git add .

--- src/Lab003.java
+++ src/Lab003.java
@@ -2,6 +2,20 @@ package src;
 
 public class Lab003 {
     public static void main(String[] args){
+    byte b=127;
+    short s=32767;
+    int i= 2147483647;
+    long phone= 9847562339L;
+    float f=32.44F;
+    double d= 13.87655567885858585;
+    char c='A';
+    char c2='_';
+    char _1='#';
+    char _2='S';
+        System.out.println(c);
+        System.out.println(c2);
+        System.out.println(_1);
+        System.out.println(d);
 
     }
 }
