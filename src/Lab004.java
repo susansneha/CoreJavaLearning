92ce7230b2737365a83e471e3a5a6e0805c1bfea
”susansneha@gmail.com” <susansneha@gmail.com>
2024-01-06 20:23:12 +0530

    git add .

---
+++ src/Lab004.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println("I am Sneha");
+        int age=35;
+        System.out.printf("My age is %d" ,age);
+    }
+}
