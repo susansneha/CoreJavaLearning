17a1fe97024de6c791b662d14024c8401f2ebf5c
”susansneha@gmail.com” <susansneha@gmail.com>
2024-01-07 15:12:54 +0530

    git add .

---
+++ src/Lab009.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        double a=15;
+        double b=4;
+        double r= a%b;
+        System.out.println(r);
+    }
+}
