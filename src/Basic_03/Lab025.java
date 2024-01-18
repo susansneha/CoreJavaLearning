f0e22c828a29df78929ae5759a5edc5d5031a6df
”susansneha@gmail.com” <susansneha@gmail.com>
2024-01-18 11:22:18 +0530

    Sneha

--- src/Basic_03/Lab025.java
+++ src/Basic_03/Lab025.java
@@ -1,4 +1,14 @@
 package src.Basic_03;
 
 public class Lab025 {
+    public static void main(String[] args) {
+
+    //Max in two numbers
+    int a=50;
+    int b=40;
+    int c=30;
+    int max= (a>b)?((a>c)?a:c):((c>b)?c:b);
+        System.out.println(max);
+    }
+
 }
