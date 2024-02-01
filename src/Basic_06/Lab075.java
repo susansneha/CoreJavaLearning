2e37e19a7d14a979f34e98db83adb5e067fd5647
”susansneha@gmail.com” <susansneha@gmail.com>
2024-02-01 16:48:58 +0530

    git add .

--- src/Basic_06/Lab075.java
+++ src/Basic_06/Lab075.java
@@ -1,7 +1,33 @@
 package src.Basic_06;
 
+import java.util.Scanner;
+
 public class Lab075 {
     public static void main(String[] args) {
-
+//prime numbers
+        int i = 0;
+        int flag = 0;
+        int m = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int n = sc.nextInt();
+ m=n/2;
+        if (n == 0 || n == 1) {
+            System.out.println("Not a prime no");
+        } else {
+            for (i = 2; i <= m; i++) {
+                if (n % i == 0) {
+                    System.out.println("Not a Prime no");
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+            if (flag == 0) {
+                System.out.println("Prime no"+n);
+            }
+        }
     }
-}
+
+
+
